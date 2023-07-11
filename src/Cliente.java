53f44bb75b21c941ededec4e13d774a3e1639677
Albertochozas <albertochozasvsk@gmail.com>
2023-07-11 18:42:37 +0200

    Clases Alberto

---
+++ src/Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente extends Persona {
+
+
+    public Cliente(String nombre, String direccion, String dni, int telefono) {
+        super(nombre, direccion, dni, telefono);
+    }
+
+
+    //public void AgregarCocheComprado(getCoche);
+
+    // public void AgregarCocheReservado(getCoche);
+}
